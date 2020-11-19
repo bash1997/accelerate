7d8d93d9ac29435d468a725682bd8850b9d95fd7
bash <bash_1997@outlook.com>
2020-11-18 21:16:04 -0500

    week 2 monday - wednesday custom post; single & archive case study pages

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -16,4 +16,30 @@ function accelerate_child_scripts(){
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+
+// Custom post types functions
+function create_custom_post_types() {
+// create a case study custom post type
+	register_post_type( 'case_studies',
+		array(
+			'labels' => array(
+				'name' => __( 'Case Studies' ),
+				'singular_name' => __( 'Case Study' )
+				),
+			'public' => true,
+			'has_archive'	=> true,
+			'rewrite' => array( 'slug' => 'case-studies' ),
+		)
+	);
+}
+
+// Hook this custom post type
+add_action( 'init', 'create_custom_post_types' );
+
+// Changes excerpt symbol
+/*function custom_excerpt_more($more) {
+	return '...<div class="read-more-link"><a  href="'. get_permalink() . '"><span>View Live Site</span> &rsaquo;</a></div>';
+}
+add_filter('excerpt_more', 'custom_excerpt_more');*/
 ?>
